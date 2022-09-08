53666f77f8873af37f64684be40a055593149110
SRA1GAVARA <92365450+SRA1GAVARA@users.noreply.github.com>
2022-09-08 11:39:45 +0530

    Add files via upload

---
+++ addition.js
@@ -0,0 +1,15 @@
+console.log("welcome GSKumar")
+var a =10;
+var b =20;
+var c = a + b;
+console.log("A Value is " + a + " and B value is " + b);
+///ES6
+console.log(`A Value is ${a} and  B value is ${b} sum is ${a+b}`);
+
+if(c==30)
+{
+    console.log("Sum is 30")
+}else
+{
+    console.log("else");
+}
